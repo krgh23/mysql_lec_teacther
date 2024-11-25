327a7d290c5d0b40e17712006d4aa7906dd09e4d
teacher-mgt <min-k-t@hanmail.net>
2024-11-25 13:18:23 +0900

    20241125

--- 05.DML.sql
+++ 05.DML.sql
@@ -14,9 +14,9 @@ INSERT INTO tbl_department(dept_name, location) VALUES ('기획부', '서울');
 
 -- tbl_employee 의 첫 번째 칼럼 emp_id 에는 NULL 값을 전달해서 emp_id 칼럼의 AUTO_INCREMENT 동작을 보장한다.
 INSERT INTO tbl_employee VALUES (NULL, 1, '구창민', '과장', 'M', '95-05-01', 5000000);
-INSERT INTO tbl_employee VALUES (NULL, 2, '김민서', '사원', 'M', '17-09-01', 2500000);
-INSERT INTO tbl_employee VALUES (NULL, 3, '이은영', '부장', 'F', '90-09-01', 5500000);
-INSERT INTO tbl_employee VALUES (NULL, 4, '한성일', '과장', 'M', '93-04-01', 5000000);
+INSERT INTO tbl_employee VALUES (NULL, 1, '김민서', '사원', 'M', '17-09-01', 2500000);
+INSERT INTO tbl_employee VALUES (NULL, 2, '이은영', '부장', 'F', '90-09-01', 5500000);
+INSERT INTO tbl_employee VALUES (NULL, 2, '한성일', '과장', 'M', '93-04-01', 5000000);
 
 -- 영구적으로 저장
 COMMIT;
